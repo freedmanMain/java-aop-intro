8334e2aadaa6b3b511a370e93ad3bb6ebf734c6f
Maksim <max.freedman.main@gmail.com>
2021-06-07 15:08:25 +0300

    added annotation.api dependency to pom.xml file

--- src/main/java/base/spring/aop/model/Library.java
+++ src/main/java/base/spring/aop/model/Library.java
@@ -1,6 +1,8 @@
 package base.spring.aop.model;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component("libraryBean")
 public class Library {
@@ -9,5 +11,15 @@ public class Library {
     }
     public void getBook() {
         System.out.println("Got the book");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Library init() method was called");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Library destroy() method was called");
     }
 }
