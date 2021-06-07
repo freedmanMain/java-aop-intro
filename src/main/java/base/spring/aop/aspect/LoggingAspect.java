756527ccd5c4bb5145d471c9184c2cf50ba7708b
Maksim <max.freedman.main@gmail.com>
2021-06-07 15:42:52 +0300

    completed LoggingAspect class implementation

--- src/main/java/base/spring/aop/aspect/LoggingAspect.java
+++ src/main/java/base/spring/aop/aspect/LoggingAspect.java
@@ -1,9 +1,20 @@
 package base.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice() {
+        System.out.println("beforeGetBookAdvice: attempt to get a book.");
+    }
+
+    @Before("execution(public void insertBook())")
+    public void beforeInsertBookAdvice() {
+        System.out.println("beforeInsertBookAdvice: attempt to insert a book.");
+    }
 }
+
--- src/main/java/base/spring/aop/config/ApplicationConfig.java
+++ src/main/java/base/spring/aop/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @EnableAspectJAutoProxy
 @ComponentScan("base.spring.aop")
 public class ApplicationConfig {
