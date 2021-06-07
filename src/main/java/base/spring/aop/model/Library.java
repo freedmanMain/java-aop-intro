fc5bf9034ed675ec3d3ddc6a7017e0f2f2ce417e
Maksim <max.freedman.main@gmail.com>
2021-06-07 14:55:39 +0300

    created Library class and completed insertBook and getBook methods

---
+++ src/main/java/base/spring/aop/model/Library.java
@@ -0,0 +1,10 @@
+package base.spring.aop.model;
+
+public class Library {
+    public void insertBook() {
+        System.out.println("Inserted the book");
+    }
+    public void getBook() {
+        System.out.println("Got the book");
+    }
+}
